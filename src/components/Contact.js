94b59b08b4e471f845aa4c50f4e9a1f14442bd95
raemillz <raeanne1991@gmail.com>
2018-06-27 13:56:32 -0400

    building out site structure

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>Contact Us</h1>
+      <h3>blablabla</h3>
+    </div>
+  )
+}
+
+export default Contact;
