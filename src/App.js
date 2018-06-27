786d71ed669517ecf6ad6f3e9109d4da6b9e16bb
raemillz <raeanne1991@gmail.com>
2018-06-27 13:53:26 -0400

    building out file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Recipes from './containers/Recipes';
+import RecipeForm from './containers/RecipeForm';
+import RecipeShow from './containers/RecipeShow';
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+            <div>
+              <Navbar/>
+              <Container>
+              <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/recipes' component={Recipes}/>
+                <Route exact path='/recipes/new' component={RecipeForm}/>
+                <Route path='/recipes/:recipeId' component={RecipeShow}/>
+                <Route exact path='/about' component={About}/>
+                <Route exact path='/contact' component={Contact}/>
+              </Switch>
+              </Container>
+            </div>
+        </Router>
       </div>
     );
   }
