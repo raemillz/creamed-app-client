52d4f031e58c582c626fc7902efb3c91e856f6b3
raemillz <raeanne1991@gmail.com>
2018-06-27 14:00:28 -0400

    building out site structure

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+    <h1> blablabla </h1>
+    <h3> blablabla </h3>
+    <h3> blablabla </h3>
+    <img src="https://images.pexels.com/photos/1174114/pexels-photo-1174114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt='' />
+    </div>
+  );
+}
+
+export default Home;
