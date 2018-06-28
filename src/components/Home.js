b0bd87bbe6e40bb07b0ba8d9a9c3bdcf9db3ce2e
raemillz <raeanne1991@gmail.com>
2018-06-28 18:08:29 -0400

    working on styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../App.css'
 import HomePageImage from './HomePageImage'
+import '../styles/homepage.css'
 
 class Home extends React.Component {
   render() {
     return (
       <div className="home">
-      <h1> blablabla </h1>
-      <h3> blablabla </h3>
-      <h3> blablabla </h3>
-      <HomePageImage/>
+        <h1 className="title"> Crème de la Cream </h1>
+        <h3> blablabla </h3>
+        <h3> blablabla </h3>
+        <HomePageImage/>
       </div>
     )
   }
--- src/components/HomePageImage.js
+++ src/components/HomePageImage.js
@@ -4,7 +4,7 @@ import '../styles/homepageimage.css'
 class HomePageImage extends React.Component {
   render () {
     return (
-      <img src="https://images.pexels.com/photos/1174114/pexels-photo-1174114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt='Sprinkles'  title="source: pexels.com" id='homePageImage' />
+      <img src="https://images.pexels.com/photos/787666/pexels-photo-787666.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt='Sprinkles'  title="source: pexels.com" id='homePageImage' />
     )
   }
 }
