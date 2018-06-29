eaded8928ad88c9dedcc20cab86d38bda636c294
raemillz <raeanne1991@gmail.com>
2018-06-29 00:19:02 -0400

    added more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ContactPageImage from './ContactPageImage';
 
 
 class Contact extends React.Component {
   render () {
     return (
-      <div className="contact">
-        <h1>Contact Us</h1>
-        <h3>blablabla</h3>
+      <div className="">
+        <h1 className="contact">Contact Us</h1>
+        <h3 className="card-title">For further inquiries or suggestions, email us at getcremedelacream@gmail.com</h3>
+        <ContactPageImage/>
       </div>
     )
   }
